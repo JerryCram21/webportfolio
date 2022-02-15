72745d2ed9f1e9e4b434cb5f3d9816444b7cd27c
Jerry Marc T. Hermoso <jthermoso@sysnetph.com>
2022-02-15 09:15:15 +0800

    v1.0.3 - added heroku default port

--- config/config.js
+++ config/config.js
@@ -3,7 +3,7 @@ const config = {};
 config.server = {};
 
 /* app settings */
-config.server.port = 3001;
+config.server.port = process.env.PORT || 3000;
 config.server.view = 'ejs';
 
 module.exports = config;
